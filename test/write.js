7bc3c4d674987c8076d9b74e02740898cc587304
Pedro Romano <pedro.romano@holidayextras.com>
2016-02-10 00:46:59 +0000

    Fix addition and removal of related resources.
    Fix addition and removal of related resources and create system test
    that cover this functionality.

--- test/write.js
+++ test/write.js
@@ -1,5 +1,6 @@
 "use strict";
 var assert = require("assert");
+var async = require("async");
 var Client = require("../.");
 require("./_testServer.js");
 
@@ -97,4 +98,88 @@ describe("Testing jsonapi-client", function() {
       done();
     });
   });
+
+  context("relations", function() {
+    it("adds and then deletes a previously unrelated resources", function(done) {
+      async.waterfall([
+        async.apply(async.parallel, {
+          article: function(callback) {
+            client.get("articles", "1be0913c-3c25-4261-98f1-e41174025ed5", callback);
+          },
+          tag: function(callback) {
+            client.get("tags", "8d196606-134c-4504-a93a-0d372f78d6c5", callback);
+          }
+        }),
+        function(res, callback) {
+          res.article.relationships("tags").add(res.tag);
+          async.waterfall([
+            function(cb) {
+              res.article.sync(cb);
+            },
+            function(cb) {
+              res.article.fetch("tags", cb);
+            },
+            function(tags, cb) {
+              assert.equal(tags.length, 2, "article was expected to have 2 tags");
+              return cb(null);
+            }
+          ], function(err) {
+            if (err) return callback(err);
+            return callback(null, res);
+          });
+        },
+        function(res, callback) {
+          res.article.relationships("tags").remove(res.tag);
+          async.waterfall([
+            function(cb) {
+              res.article.sync(cb);
+            },
+            function(cb) {
+              res.article.fetch("tags", cb);
+            },
+            function(tags, cb) {
+              assert.equal(tags.length, 1, "article was expected to have 1 tag");
+              return cb(null);
+            }
+          ], callback);
+        }
+      ], function(err) {
+        assert.equal(err, null);
+        done();
+      });
+    });
+  });
+
+  context("relations", function() {
+    it("doesn't duplicate related resources added multiple times", function(done) {
+      async.waterfall([
+        async.apply(async.parallel, {
+          article: function(callback) {
+            client.get("articles", "1be0913c-3c25-4261-98f1-e41174025ed5", callback);
+          },
+          tag: function(callback) {
+            client.get("tags", "2a3bdea4-a889-480d-b886-104498c86f69", callback);
+          }
+        }),
+        function(res, callback) {
+          res.article.relationships("tags").add(res.tag);
+          res.article.sync(function(err) {
+            if (err) callback(err);
+            return callback(null, res.article);
+          });
+        },
+        function(article, callback) {
+          article.fetch("tags", callback);
+        },
+        function(tags, callback) {
+          assert.equal(tags.length, 1, "article was expected to only have 1 tag");
+          return callback(null);
+        }
+      ], function(err) {
+        assert.equal(err, null);
+        done();
+      });
+    });
+  });
+
 });
