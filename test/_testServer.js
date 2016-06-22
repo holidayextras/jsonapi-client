0f15954fb9474230eb6e01ae1c7b9c3aa3ca68b7
theninj4 <oliver.rumbelow@holidayextras.com>
2016-06-22 11:55:25 +0100

    v0.6.0 - Updating dependencies

--- test/_testServer.js
+++ test/_testServer.js
@@ -3,6 +3,13 @@
 var jsonApiTestServer = require("jsonapi-server/example/server.js");
 if (!jsonApiTestServer.start) return;
 
+jsonApiTestServer.getExpressServer().use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 before(function() {
   jsonApiTestServer.start();
 });
